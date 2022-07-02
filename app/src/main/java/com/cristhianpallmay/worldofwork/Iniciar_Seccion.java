ce8e751d83748c7987d2d1ea4d17e06d65ad9203
Cristhian Pallmay <cristhian.pallmay.est@tecazuay.edu.ec>
2022-07-01 19:19:52 -0500

    Commit de iniciar seccion.

---
+++ app/src/main/java/com/cristhianpallmay/worldofwork/Iniciar_Seccion.java
@@ -0,0 +1,14 @@
+package com.cristhianpallmay.worldofwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Iniciar_Seccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_seccion);
+    }
+}
