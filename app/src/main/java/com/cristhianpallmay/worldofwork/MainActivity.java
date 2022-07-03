a9c4568e046266fe30eb4a196916bf72f2bf5153
Pablo0925 <pablosumba2000@gmail.com>
2022-07-03 18:45:27 -0500

    Creadas las activitys de perfil de usuario, Ofrecer servicio, Mensajes informativos sobre actualizacion de datos de usuario, editar datos de usuario y mensajes

--- app/src/main/java/com/cristhianpallmay/worldofwork/MainActivity.java
+++ app/src/main/java/com/cristhianpallmay/worldofwork/MainActivity.java
@@ -2,13 +2,30 @@ package com.cristhianpallmay.worldofwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button BtnOfrecerServicio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    setContentView(R.layout.activity_main);
+    //setContentView(R.layout.activity_perfil_usuario_n);
+    BtnOfrecerServicio=(Button)findViewbyId(R.id.BtnOfrecerServicio);
+
+    BtnOfrecerServicio.setOnClickListener(new View.OnClickListener(){
+
+
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(packageContext: MainActivity.this, activity_ofrecer_servicio.class);
+            startActivity(i);
+        }
+    });
+}
 }
---
+++ app/src/main/java/com/cristhianpallmay/worldofwork/Msn_Informativo_MOD_USER.java
@@ -0,0 +1,14 @@
+package com.cristhianpallmay.worldofwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Msn_Informativo_MOD_USER extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msn_informativo_mod_user);
+    }
+}
